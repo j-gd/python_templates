02db5046b50d60b609aacf1d28f80e4aa9d5e696
J-G Demathieu <jdemathieu@gmail.com>
2019-08-16 18:30:45 -0700

    Add autoreload tests

---
+++ check.py
@@ -0,0 +1,10 @@
+# Find a way to use a global variable or 
+
+def c(data):
+  try:
+    if check:
+      print(data)
+  except:
+    pass
+  else:
+      check = False
---
+++ practice/reload_test.py
@@ -0,0 +1,18 @@
+# hello.py
+
+
+def hello_world():
+    print("hello, jg world!")
+
+
+def add_em_up(a, b, c):
+    return a + b + c
+
+
+def power_up(b, e):
+    return b**e
+
+
+if __name__ == "__main__":
+    hello_world()
+    print(add_em_up(3, 4, 5))
